cdf1d96c74b01518eaebc0ee79c57ec3dd901f4e
dhruvnaik111 <68032436+dhruvnaik111@users.noreply.github.com>
2020-08-13 10:01:22 -0700

    Updates

---
+++ precache-manifest.065db47741bf38c6c3d715c5a9d2cb9b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfa0be33161c80fbec9fdfe679465ba",
+    "url": "/KoaanSite/index.html"
+  },
+  {
+    "revision": "63d0f79021f8cb065ce3",
+    "url": "/KoaanSite/static/css/main.fc0f4148.chunk.css"
+  },
+  {
+    "revision": "e9268615b868b339ce58",
+    "url": "/KoaanSite/static/js/2.2d77933c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KoaanSite/static/js/2.2d77933c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d0f79021f8cb065ce3",
+    "url": "/KoaanSite/static/js/main.9e081932.chunk.js"
+  },
+  {
+    "revision": "93fad6618583fcea5dad",
+    "url": "/KoaanSite/static/js/runtime-main.517ee443.js"
+  },
+  {
+    "revision": "350872b6b114ec11a686b40f7f809828",
+    "url": "/KoaanSite/static/media/TALL.350872b6.png"
+  },
+  {
+    "revision": "f019e0f1f3dd7e4bccc1cfe4d65e9462",
+    "url": "/KoaanSite/static/media/dynastytxt.f019e0f1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/KoaanSite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d40e95dc14311378e51f9a561b9002df",
+    "url": "/KoaanSite/static/media/paintingtree-min.d40e95dc.png"
+  },
+  {
+    "revision": "d1fe74e3bd09657a55ef248d996752a8",
+    "url": "/KoaanSite/static/media/wide.d1fe74e3.png"
+  }
+]);
